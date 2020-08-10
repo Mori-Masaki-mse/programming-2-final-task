a552bbac3055203f00c8ff916b6414ce4bef0262
Mori-Masaki-mse <43803217+Mori-Masaki-mse@users.noreply.github.com>
2020-08-10 14:56:20 +0900

    Add files via upload

--- 成績の降順、合計、平均.cpp
+++ 成績の降順、合計、平均.cpp
@@ -5,9 +5,10 @@
 #include <string>
 using namespace std;
 
+//関数の定義
 void AveScore(int a,int b,int c,int d,int e) {
-	int sum;
-	int ave;
+	int sum;//合計の変数の定義
+	int ave;//平均の変数の定義
 
 	sum = a + b + c + d + e;
 	ave = sum / 5.0;
@@ -21,19 +22,23 @@ int main() {
 	int d = 76;
 	int e = 55;
 
+	//ファイルの出力
 	ofstream outfile("成績表.txt");
 	outfile << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << endl;
 
+	//ファイルの入力
 	ifstream infile("成績表.txt");
 	string str;
 	while (infile >> a >> b >> c >> d >> e) {
+		//空のvecotrの構築
 		vector<int>v1;
-		v1.push_back(a);
+		v1.push_back(a);//要素の追加
 		v1.push_back(b);
 		v1.push_back(c);
 		v1.push_back(d);
 		v1.push_back(e);
 
+		//降順に並び替え
 		sort(v1.rbegin(), v1.rend());
 		for (auto i : v1)
 			cout << i << endl;
